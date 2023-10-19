25bc577b30962f8df622320a117d0314c56cec36
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-10-19 15:59:30 +0300

    ---

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+const player = new Player(iframe);
 
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+  //   {
+  //     duration: 61.857;
+  //     percent: 0.049;
+  //     seconds: 3.034;
+  //   }
+  localStorage.setItem('videoplayer-current-time', JSON.parse(data.seconds));
+};
+
+player.on('timeupdate', onPlay);
