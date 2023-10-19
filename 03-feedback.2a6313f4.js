844008f5bf36ccf8a694c5614e69dd8e10598758
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-10-19 18:45:23 +0000

    Deploying to gh-pages from @ KaterynaPryimakova/goit-js-hw-08@961f0db69b9a2e0eaae51dca7bb63f90115e61fe 🚀

--- 03-feedback.2a6313f4.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("input",(function(e){e.target.value}));
-//# sourceMappingURL=03-feedback.2a6313f4.js.map
--- 03-feedback.a43d0cfb.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("input",(function(e){e.target.value}));
-//# sourceMappingURL=03-feedback.a43d0cfb.js.map
