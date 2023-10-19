c61bb7be7bf12b2be77e83d65bf8020167426837
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-10-19 20:20:15 +0300

    ---

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,8 @@
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', onInput);
 
+function onInput(event) {
+  event.target.value;
+}
+
+// localStorage.setItem("feedback-form-state", JSON.stringify())
