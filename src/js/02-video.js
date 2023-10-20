51e3acc691a98a5db6b3ee8371843a4f9542a92b
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-10-20 19:32:56 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,4 +10,6 @@ const onPlay = function (data) {
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
